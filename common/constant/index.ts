696158a3ed8b50da5324710d0e2070e0304591f7
Abdi Purnawan <abdipurnawan1@gmail.com>
2024-07-05 08:29:34 +0800

    feat[profile-component]: change profile picture

--- common/constant/index.ts
+++ common/constant/index.ts
@@ -4,5 +4,5 @@ export const DEVTO_PROFILE = 'https://dev.to/abdipurnawan';
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/';
 export const PAGESPEED_CATEGORIES = '&category=accessibility&category=performance&category=best-practices&category=seo';
 export const PROFILE_URL =
-  'https://res.cloudinary.com/dmfwgcart/image/upload/v1694227223/F516027D-7791-414F-9E01-13AF91ABC453_1_201_a_chxf65.jpg';
+  'https://res.cloudinary.com/dmfwgcart/image/upload/v1720139270/xlohlu7c9xjph8uwkqgy.png';
 export const SAWERIA_URL = 'https://saweria.co/abdipurnawan';
